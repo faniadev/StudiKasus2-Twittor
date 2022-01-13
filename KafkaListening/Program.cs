0ffe436fdd761a630cbdb6004e2c91f985933411
fania devira <faniardelia@gmail.com>
2022-01-13 15:04:15 +0700

    scaffold

---
+++ KafkaListening/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace KafkaListening
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
